8f6deb19f64821eb697922eef6209c160bd92472
Mario Martín <mmartin.mrmg@gmail.com>
2024-02-29 09:45:16 +0100

    style: 💄 Add Style, Logo and README

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'darkBlack': '#1E1E1E',
+      'black': '#2d2d2d',
+      'overlayBlack': 'rgba(0, 0, 0, 0.9)',
+      'gray': '#454545',
+      'white': '#FFFFFF',
+      'bone': '#e0e0e0',
+      'darkGreen': '#345e37',
+      'green': '#2E8B57',
+      'lowGreen': '#61bc84',
+      'lowerGreen': '#8FBC8F',
+      'lightGreen': '#c6ffe6',
+      'overlayGreen': '#345e3748'
+    }
+  },
+  plugins: []
+}
